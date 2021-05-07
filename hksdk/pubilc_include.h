669300d395fd848456dbf958bb15dbc8fcbd4d65
DESKTOP-MRQ32EM\Administrator <yanjuan9998@gmail.com>
2021-05-08 02:00:32 +0800

    add color-out

--- hksdk/pubilc_include.h
+++ hksdk/pubilc_include.h
@@ -1,6 +1,10 @@
 #pragma once
+
 #include "../xorstr/include/xorstr.hpp"
 #include "../lazy_importer/include/lazy_importer.hpp"
 #include "../portable-executable-library/pe_lib/pe_bliss.h"
+#include "../AES/src/AES.h"
+#include "../colored-cout/colored_cout.h"
+
 #include "string_convert.h"
 #include "file.h"
